281c0f69b4336ae1e3bf8be2653378e1b1adc5f8
YFS11013 <83509926@qq.com>
2022-06-22 23:26:05 +0800

    移除Duende

--- src/Blazor.Server.UI/ConfigureServices.cs
+++ src/Blazor.Server.UI/ConfigureServices.cs
@@ -54,7 +54,7 @@ public static class ConfigureServices
         services.AddScoped<IUserPreferencesService, UserPreferencesService>();
         services.AddScoped<IMenuService, MenuService>();
         services.AddTransient<INotificationService, InMemoryNotificationService>();
-        services.AddGoogleAnalytics("G-PRYNCB61NV");
+        //services.AddGoogleAnalytics("G-PRYNCB61NV");
 
         return services;
     }
